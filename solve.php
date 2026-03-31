e408d39bd0ffc31758604d07e915cfb0122a3bc3
Alex Miles <ghostaldev@gmail.com>
2026-03-31 12:10:32 +0100

    Move Solver class to src directory

---
+++ solve.php
@@ -0,0 +1,21 @@
+<?php
+
+use Street\Street;
+
+spl_autoload_register(function ($class) {
+
+    // base directory for the namespace prefix
+    $base_dir = __DIR__ . '/src/';
+
+    // replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the relative class name, append
+    // with .php
+    $file = $base_dir . str_replace('\\', '/', $class) . '.php';
+
+    // if the file exists, require it
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+
+new Solver(new Street());
